10f273a432a7231e8cdd060faf6cb48e032eb58f
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-04-28 15:48:03 -0700

    Complete getHumanChoice

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function getComputerChoice() {
   }
   return computerChoice; 
 }
+
+function getHumanChoice() {
+  let humanChoice = prompt("Enter your choice: rock, paper, or scissors: "); 
+  humanChoice = humanChoice.trim().toLowerCase(); 
+  while(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+    humanChoice = prompt("Invalid entry. Please enter rock, paper, or scissors"); 
+    humanChoice = humanChoice.trim().toLowerCase(); 
+  }
+  return humanChoice; 
+}
+
+console.log(getHumanChoice()); 
