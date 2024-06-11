34f76d41fa6a2ed1295673322821a246d062042a
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-06-10 17:35:32 -0700

    Change from console to webpage display

--- script.js
+++ script.js
@@ -1,60 +1,75 @@
 const NUM_ROUNDS = 5; 
 let humanScore = 0; 
 let computerScore = 0; 
+let roundNum = 1; 
 
-function getComputerChoice() {
-  let computerChoice = ""; 
-  const randomValue = Math.floor(Math.random() * 3); 
-
-  if(randomValue === 0) {
-    computerChoice = "rock"; 
-  } else if (randomValue === 1) {
-    computerChoice = "paper"; 
-  } else {
-    computerChoice = "scissors"; 
-  }
-  return computerChoice; 
-}
-
-function getHumanChoice() {
-  let humanChoice = prompt("Enter your choice: rock, paper, or scissors: "); 
-  humanChoice = humanChoice.trim().toLowerCase(); 
-  while(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-    humanChoice = prompt("Invalid entry. Please enter rock, paper, or scissors"); 
-    humanChoice = humanChoice.trim().toLowerCase();  //all lowercase for easy comparisons
-  }
-  return humanChoice; 
-}
+const rockBtn = document.querySelector(".container .rock"); 
+const paperBtn = document.querySelector(".container .paper"); 
+const scissorBtn = document.querySelector(".container .scissors"); 
+const resultList = document.querySelector(".results"); 
 
 
-function playGame() {
-  for(let i = 1; i <= NUM_ROUNDS; ++i) {
-    playRound(getHumanChoice(), getComputerChoice()); 
-  }
-  if(humanScore > computerScore) {
-    console.log(`CONGRATULATIONS! You won with a score of ${humanScore} - ${computerScore}`); 
-  } else if(computerScore > humanScore) {
-    console.log(`Game over! You LOST with a score of ${humanScore} - ${computerScore}`); 
-  } else {
-    console.log(`You and the computer TIED with a score of ${humanScore} - ${computerScore}`); 
+  rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice()); 
+  }); 
+  paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice()); 
+  }); 
+  scissorBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice()); 
+  }); 
+
+  function getComputerChoice() {
+    let computerChoice = ""; 
+    const randomValue = Math.floor(Math.random() * 3); 
+
+    if(randomValue === 0) {
+      computerChoice = "rock"; 
+    } else if (randomValue === 1) {
+      computerChoice = "paper"; 
+    } else {
+      computerChoice = "scissors"; 
+    }
+    return computerChoice; 
   }
 
   function playRound(humanChoice, computerChoice) {
-    console.log(`The computer chose: ${computerChoice}`);  //so user can know clearly what computer chose
+    const winnerMessage = document.createElement("p"); 
+    const scoreUpdate = document.createElement("p"); 
+    winnerMessage.textContent = `Round ${roundNum}: The computer chose ${computerChoice}. `;  //user knows clearly what computer chose
     if(humanChoice === computerChoice) {
-      console.log(`It's a TIE! (You both chose ${humanChoice})`); 
+      winnerMessage.textContent += `It's a TIE! (You both chose ${humanChoice})`; 
     } 
     else if(humanChoice === "scissors" && computerChoice === "paper" || 
             humanChoice === "paper" && computerChoice === "rock" || 
             humanChoice === "rock" && computerChoice === "scissors") {
-      console.log(`You WIN! (${humanChoice} beats ${computerChoice})`); 
+      winnerMessage.textContent += `You WIN! (${humanChoice} beats ${computerChoice})`; 
       humanScore++; 
     }
     else {
-      console.log(`You LOSE! (${computerChoice} beats ${humanChoice})`); 
+      winnerMessage.textContent += `You LOSE! (${computerChoice} beats ${humanChoice})`; 
       computerScore++; 
     }
+    scoreUpdate.textContent = `Current score is ${humanScore} - ${computerScore}`; 
+    resultList.appendChild(winnerMessage); 
+    resultList.appendChild(scoreUpdate); 
+    roundNum++; 
+    if(roundNum === 5) endGame(); 
   }
+
+
+function endGame() {
+  const finalScoreMsg = document.createElement("p"); 
+  finalScoreMsg.textContent = "GAME OVER! "
+  if(humanScore > computerScore) {
+    finalScoreMsg.textContent += `You WON ${humanScore} - ${computerScore}`; 
+  }
+  else if(humanScore < computerScore) {
+    finalScoreMsg.textContent += `You LOST ${humanScore} - ${computerScore}`; 
+  }
+  else {
+    finalScoreMsg.textContent += `You TIED ${humanScore} - ${computerScore}`; 
+  }
+  resultList.appendChild(finalScoreMsg); 
 }
 
-playGame(); 
